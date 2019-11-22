f7701383f27eb451efdcd7f4dca1634a6a88b84e
Chuyi Zhang <chuyi_zhang@outlook.com>
2019-11-22 01:09:10 -0800

    Fix bugs

--- main.s
+++ main.s
@@ -4,7 +4,7 @@
     Title:
         .ascii "====================\n"
         .ascii "CALIFORNIA GOLD RUSH\n"
-        .ascii "====================\0"
+        .ascii "====================\n\0"
 
     Rules:
         .ascii "Rules:\n"
@@ -12,7 +12,7 @@
         .ascii "2. Your endurance drops 10-25% each week. If it reaches 0%, the game ends.\n"
         .ascii "3. Panning for gold yields 0-100 dollars.\n"
         .ascii "4. A sluice yields 0-1000 dollars (durability drops 20-50% each week)\n"
-        .ascii "5. Food costs 30-50 dollars.\0"
+        .ascii "5. Food costs 30-50 dollars.\n\0"
 
 .text
 .global main
