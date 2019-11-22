c3dd2aab1c06cc830b13bc60e405f00a69d4e596
Chuyi Zhang <chuyi_zhang@outlook.com>
2019-11-22 00:51:02 -0800

    Print title and rules

---
+++ main.s
@@ -0,0 +1,31 @@
+.extern puts
+
+.data
+    Title:
+        .ascii "====================\n"
+        .ascii "CALIFORNIA GOLD RUSH\n"
+        .ascii "====================\0"
+
+    Rules:
+        .ascii "Rules:\n"
+        .ascii "1. 20 weeks (5 months)\n"
+        .ascii "2. Your endurance drops 10-25% each week. If it reaches 0%, the game ends.\n"
+        .ascii "3. Panning for gold yields 0-100 dollars.\n"
+        .ascii "4. A sluice yields 0-1000 dollars (durability drops 20-50% each week)\n"
+        .ascii "5. Food costs 30-50 dollars.\0"
+
+.text
+.global main
+main:
+    # Print Title and Rules
+    mov $Title, %rdi
+    call puts
+    mov $'\n', %rdi
+    call puts
+    mov $Rules, %rdi
+    call puts
+
+    # End Program
+    mov $3, %rax
+    mov $0, %rdi
+    syscall
