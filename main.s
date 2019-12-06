4c529812c571fff6790b616ed5dccf6ab8e416a6
Chuyi Zhang <chuyi_zhang@outlook.com>
2019-12-06 15:45:01 -0800

    Add main loop

--- main.s
+++ main.s
@@ -3,6 +3,14 @@
 .text
     main:
         call PrintInformation
+    
+    BeginLoop:
+        cmpq $20, Week
+        jg EndLoop
+        call RunWeek
+        addq $1, Week
+        jmp BeginLoop
+    EndLoop:
 
         # End Program
         mov $60, %rax
