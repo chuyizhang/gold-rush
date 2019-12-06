2ec59fa771c7b616b96b98fc50282c6422b1cfdd
Chuyi Zhang <chuyi_zhang@outlook.com>
2019-12-06 15:29:59 -0800

    Add data.s

---
+++ data.s
@@ -0,0 +1,17 @@
+.global Week
+.global Money
+.global Endurance
+.global Sluice
+
+.data
+    Week:
+        .quad 1
+    
+    Money:
+        .quad 100
+    
+    Endurance:
+        .quad 100
+    
+    Sluice:
+        .quad 100
