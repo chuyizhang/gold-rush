85bd20d3a377104bbe3e9e2fa6207cbdabd9f7e3
Chuyi Zhang <chuyi_zhang@outlook.com>
2019-12-06 14:22:44 -0800

    Print Information

---
+++ information.s
@@ -0,0 +1,24 @@
+.extern puts
+
+.data
+    Title:
+        .ascii "====================\n"
+        .ascii "CALIFORNIA GOLD RUSH\n"
+        .ascii "====================\n\0"
+
+    Rules:
+        .ascii "Rules:\n"
+        .ascii "1. 20 weeks (5 months)\n"
+        .ascii "2. Your endurance drops 10-25% each week. If it reaches 0%, the game ends.\n"
+        .ascii "3. Panning for gold yields 0-100 dollars.\n"
+        .ascii "4. A sluice yields 0-1000 dollars (durability drops 20-50% each week)\n"
+        .ascii "5. Food costs 30-50 dollars.\n\0"
+
+.text
+PrintInformation:
+    push %rdi
+    mov $Title, %rdi
+    call puts
+    mov $Rules, %rdi
+    call puts
+    pop %rdi
